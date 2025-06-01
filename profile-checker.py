b1f125694c272cb1fb3842ae6b6f46d65c2c9921
Christian Tannoury <tsmchris42@gmail.com>
2025-06-01 16:10:57 +0300

    Prompting & Printing the User Input

---
+++ profile-checker.py
@@ -0,0 +1,18 @@
+# Getting input from the user 
+print("**** Waiting for User Information ****")
+name = input("Enter your name: ")
+age = int(input("Enter your age: "))
+gpa = float(input("Enter your GPA(0.0 -> 5.0): "))
+interest = input("Enter your field of interest: ")
+graduated = input("Are you graduated: 'Yes' or 'No'? ")
+
+# Printing the user input in a neat format (First Part)
+print("\n**** Displaying User Information ****")
+print("Name:", name)
+print("Age:", age)
+print("GPA:", gpa)
+print("Field of Interest:", interest)
+print("Graduated:", graduated)
+
+
+
